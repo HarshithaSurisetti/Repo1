506ed0e3cec95f74326d29d6d61d1d02c95a3c25
Harshi <bt1999hs@gmail.com>
2024-10-15 12:54:38 +0530

    first comment

---
+++ src/project1/One.java
@@ -0,0 +1,16 @@
+package project1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class One {
+
+	public static void main(String[] args) {
+//   System.setProperty("webdriver.edge.driver","C:\\Users\\Harsh\\Documents\\project1\\Drivers\\msedgedriver.eve");
+	 WebDriver driver = new FirefoxDriver();
+	}
+	
+	
+}
